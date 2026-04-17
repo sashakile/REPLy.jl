c4a39d2d89d849ff3cdabd22303d7677654c10e7
sashakile <saxa.kile@gmail.com>
2026-04-17 17:59:57 -0300

    chore: ignore local spec and scaffold Julia package

---
+++ src/REPLy.jl
@@ -0,0 +1,11 @@
+module REPLy
+
+export protocol_name, version_string
+
+"""Return the canonical protocol name for this package."""
+protocol_name() = "REPLy"
+
+"""Return a human-readable package version string."""
+version_string() = string(pkgversion(REPLy))
+
+end
